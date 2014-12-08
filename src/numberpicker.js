12e0daae6d66b79d622b4b28f967b41a04a31f0b
bmaupin <bmaupincode@gmail.com>
2014-12-08 14:38:27 -0500

    Commit progress

--- src/numberpicker.js
+++ src/numberpicker.js
@@ -1,7 +1,41 @@
 angular.module('angular-bootstrap-numberpicker', [])
 
-.directive('numberpicker', function () {
+.constant('numberpickerConfig', {
+  defaultValue: 0,
+  valueStep: 1,
+})
+
+.factory('NumberPickerSvc', function() {
   return {
+  };
+})
+
+.controller('NumberpickerCtrl', ['$scope', function($scope) {
+  this.init = function(ngModelCtrl) {
+    this.ngModelCtrl = ngModelCtrl;
+    
+    console.log(ngModelCtrl.$modelValue);
+    console.log(ngModelCtrl.$viewValue);
+  };
+  
+  $scope.updateValue = function() {
+    console.log($scope.pickerValue);
+    console.log(this.ngModelCtrl.$modelValue);
+//    console.log(Object.keys($scope));
+  };
+}])
+
+.directive('numberpicker', function() {
+  return {
+    require: ['numberpicker', '?^ngModel'],
+    controller:'NumberpickerCtrl',
     templateUrl: 'src/numberpicker.html',
+    link: function(scope, element, attrs, ctrls) {
+      var numberpickerCtrl = ctrls[0], ngModelCtrl = ctrls[1];
+
+      if (ngModelCtrl) {
+        numberpickerCtrl.init(ngModelCtrl);
+      }
+    },
   };
 });
