cc27b8e4c21c4d2e674e7ddbb070f4b583074b72
bmaupin <bmaupincode@gmail.com>
2014-12-16 13:56:47 -0500

    Try a much simpler method of syncing the template model with the parent, at least for now

--- src/numberpicker.js
+++ src/numberpicker.js
@@ -11,28 +11,11 @@ angular.module('angular-bootstrap-numberpicker', [])
 })
 */
 .controller('NumberpickerCtrl', ['$scope', function($scope) {
-  ngModelCtrl = { $setViewValue: angular.noop }; // nullModelCtrl
-
-  this.init = function(_ngModelCtrl) {
-    ngModelCtrl = _ngModelCtrl;
-  };
-  
-  $scope.updateValue = function() {
-    ngModelCtrl.$setViewValue($scope.pickerValue);
-  };
 }])
 
 .directive('numberpicker', function() {
   return {
-    require: ['numberpicker', '?^ngModel'],
     controller:'NumberpickerCtrl',
     templateUrl: 'src/numberpicker.html',
-    link: function(scope, element, attrs, ctrls) {
-      var numberpickerCtrl = ctrls[0], ngModelCtrl = ctrls[1];
-
-      if (ngModelCtrl) {
-        numberpickerCtrl.init(ngModelCtrl);
-      }
-    },
   };
 });
