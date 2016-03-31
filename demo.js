c80b210b243058333dc080972ad50fcd801778db
bmaupin <bmaupin@users.noreply.github.com>
2016-03-31 13:50:59 -0400

    Demonstrate ng-change and changing some options

--- demo.js
+++ demo.js
@@ -1,1 +1,9 @@
-angular.module('numberpicker-demo', ['angularBootstrapNumberpickerSource']).controller('NumberpickerDemoCtrl', function () {});
+angular.module('numberpicker-demo', ['angularBootstrapNumberpickerSource'])
+  .controller('NumberpickerDemoCtrl', function ($scope, $log) {
+    $scope.defaultValue = 5;
+    $scope.min = 0;
+
+    $scope.valueChanged = function() {
+      $log.log('Value changed=' + $scope.numberValue);
+    };
+  });
